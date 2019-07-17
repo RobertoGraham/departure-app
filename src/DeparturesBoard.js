467397334de6b9c306ac250ef98411e7e8d509bb
Roberto Graham <robertogrhm@gmail.com>
2019-07-17 12:25:39 +0100

    Style changes to departures board

--- src/DeparturesBoard.js
+++ src/DeparturesBoard.js
@@ -2,10 +2,6 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Store } from './Store';
 import { Typography } from '@rmwc/typography';
 import '@material/typography/dist/mdc.typography.css';
-import { Card } from '@rmwc/card';
-import '@material/card/dist/mdc.card.css';
-import '@material/button/dist/mdc.button.css';
-import '@material/icon-button/dist/mdc.icon-button.css';
 
 function DeparturesBoard({ match }) {
     const [fetchingBusStopDepartures, setFetchingBusStopDepartures] = useState(false);
@@ -30,37 +26,35 @@ function DeparturesBoard({ match }) {
     }, [busStopDepartures, dispatch, fetchingBusStopDepartures, id]);
 
     const busStop = busStops.find(busStop => busStop.id === id);
-    const departure = busStopDepartures[id];
+    const departures = busStopDepartures[id];
     const titleText = busStop ? busStop.name : `No bus stop found with id: ${id}`;
     const subtitleText = busStop ? busStop.locality : '';
     return (
-        <Card>
-            <div style={{ padding: '0 1rem' }}>
-                <Typography
-                    use="headline6"
-                    tag="h2"
-                    theme="textPrimaryOnBackground"
-                    style={{
-                        whiteSpace: 'wrap',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis'
-                    }}>
-                    {titleText}
-                </Typography>
-                <Typography
-                    use="subtitle2"
-                    tag="h3"
-                    theme="textSecondaryOnBackground"
-                    style={{
-                        marginTop: '-1rem',
-                        whiteSpace: 'wrap',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis'
-                    }}>
-                    {subtitleText}
-                </Typography>
-            </div>
-        </Card>
+        <header style={{ padding: '0 1rem', textAlign: 'center' }}>
+            <Typography
+                use="headline6"
+                tag="h2"
+                theme="textPrimaryOnBackground"
+                style={{
+                    whiteSpace: 'wrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis'
+                }}>
+                {titleText}
+            </Typography>
+            <Typography
+                use="subtitle2"
+                tag="h3"
+                theme="textSecondaryOnBackground"
+                style={{
+                    marginTop: '-1rem',
+                    whiteSpace: 'wrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis'
+                }}>
+                {subtitleText}
+            </Typography>
+        </header>
     );
 }
 
